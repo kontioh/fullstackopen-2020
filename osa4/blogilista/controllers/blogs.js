1cfd9235e8f33be0c6cc8a1c625491932dbd2747
Helmiina Kontio <helmiina.kontio@gmail.com>
2020-12-02 18:40:22 +0200

    Revert "blogilistan testit 4.13-4.14"
    This reverts commit b45fd57b8e95dd48b233ec6b24c5308d3f3444c1.

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,14 +1,12 @@
 const blogsRouter = require('express').Router()
-const { request, response } = require('express')
 const Blog = require('../models/blog')
 
-// GET
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
   
-// POST
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -22,27 +20,5 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
 })
-
-// DELETE
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
-})
-
-// PUT
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
-
-  const updatedBlog = await Blog
-    .findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
-  response.json(updatedBlog.toJSON())
-})
-
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -57,17 +57,8 @@ const blogsInDb = async() => {
   return blogs.map(blog => blog.toJSON())
 }
 
-const nonExistingId = async () => {
-  const blog = new Blog({ title: "X", author: "Y", url: "Z" })
-  await blog.save()
-  await blog.remove
-
-  return blog._id.toString()
-}
-
 
 module.exports = {
   initialBlogs,
-  blogsInDb,
-  nonExistingId
+  blogsInDb
 }
