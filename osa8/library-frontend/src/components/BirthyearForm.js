9d36b16671b973b32402e26cce4e1f0411867d6c
Helmiina Kontio <helmiina.kontio@gmail.com>
2020-12-28 14:41:47 +0200

    kirjaston frontend 8.8-8.12

---
+++ osa8/library-frontend/src/components/BirthyearForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+import { ALL_AUTHORS, SET_BIRTHYEAR } from '../queries'
+
+const BirthyearForm = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [updateAuthor] = useMutation(SET_BIRTHYEAR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
+  const result = useQuery(ALL_AUTHORS)
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    updateAuthor({ variables: { name, born } })
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        name
+        <select value={name} onChange={({ target }) => setName(target.value)}>
+          {result.data.allAuthors.map((a,i) => <option key={i} value={a.name}>{a.name}</option>)}
+        </select>
+        <div>
+          born<input type='number' value={born} onChange={({ target }) => setBorn(Number(target.value))} />  
+        </div>
+        <div>
+          <button type='submit'>update author</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+/*
+<div>
+          name <input value={name} onChange={({ target }) => setName(target.value)} />
+        </div>
+*/
+
+export default BirthyearForm
